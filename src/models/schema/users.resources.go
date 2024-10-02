922d01c187b3774e5786e3350559f002943ee59a
Garvit <garvit.jain@pw.live>
2024-10-02 22:46:06 +0530

    resource example

--- src/models/schema/users.resources.go
+++ src/models/schema/users.resources.go
@@ -2,14 +2,16 @@ package schema
 
 import (
 	"context"
+	"errors"
 	"project/src/models/ent"
 	"project/src/models/ent/company"
+	"project/src/models/ent/user"
 )
 
 func (Company) Resource() *ent.CompanyResource {
 	return &ent.CompanyResource{
 		HasReadPermission: func(ctx context.Context, jwtUser *ent.User, client *ent.CompanyQuery) error {
-			client.Where(company.ID(jwtUser.CompanyID))
+			client.Where(company.ID(jwtUser.CompanyID)) // only allow the company members to read the company
 			return nil
 		},
 		HasCreatePermission: func(ctx context.Context, jwtUser *ent.User, client []*ent.CreateCompanyInput) error {
@@ -21,6 +23,7 @@ func (Company) Resource() *ent.CompanyResource {
 func (User) Resource() *ent.UserResource {
 	return &ent.UserResource{
 		HasReadPermission: func(ctx context.Context, jwtUser *ent.User, client *ent.UserQuery) error {
+			client.Where(user.CompanyID(jwtUser.CompanyID))
 			return nil
 		},
 		HasCreatePermission: func(ctx context.Context, jwtUser *ent.User, client []*ent.CreateUserInput) error {
@@ -30,10 +33,13 @@ func (User) Resource() *ent.UserResource {
 			return nil
 		},
 		HasDeletePermission: func(ctx context.Context, jwtUser *ent.User, client *ent.User) error {
-			return nil
+			return errors.New("you cannot delete users")
 		},
 		HasUpdatePermission: func(ctx context.Context, jwtUser *ent.User, member *ent.User, update *ent.UpdateUserInput) error {
-			return nil
+			if jwtUser.ID == member.ID {
+				return nil
+			}
+			return errors.New("you cannot update other users")
 		},
 	}
 }
